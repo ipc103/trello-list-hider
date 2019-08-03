0954b4a4ad04dbb915d120cf8cb02e27288b6c6e
Ian Candy <iancandysemail@gmail.com>
2019-08-03 19:38:23 -0400

    refactor for readability

--- content.js
+++ content.js
@@ -1,39 +1,47 @@
 console.log("Trello List Hider Loaded!")
 
-const minimizeList = (event) => {
-  const targetClass = '.list-header';
-  const toHideClass = 'list-cards';
-  const placeHolderClass = 'trello-list-hider--minimized';
-  const placeHolderText = 'Click to Expand'
+const targetClass = '.list-header';
+const toHideClass = 'list-cards';
+const placeHolderClass = 'trello-list-hider--minimized';
+const placeHolderText = '...'
 
+
+const addPlaceHolderTo = (listHeader) => {
+  let placeHolder = listHeader.getElementsByClassName(placeHolderClass)[0];
+
+  if (placeHolder === undefined){
+    placeHolder = document.createElement('div')
+    placeHolder.classList.add(placeHolderClass);
+    placeHolder.innerText = placeHolderText
+    placeHolder.style.display = 'none';
+    listHeader.append(placeHolder)
+  }
+
+  return placeHolder;
+}
+
+const toggleHidden = (listCards, placeHolder) => {
+  const hidden = 'none';
+  const shown = '';
+
+  if ( listCards.style.display === hidden){
+    listCards.style.display = shown;
+    placeHolder.style.display = hidden;
+  } else {
+    listCards.style.display = hidden;
+    placeHolder.style.display = shown;
+  }
+}
+
+const minimizeList = (event) => {
   if( event.target.matches(targetClass) ) {
     const listHeader = event.target;
+    const placeHolder = addPlaceHolderTo(listHeader);
+
     const list = listHeader.parentElement;
-
-    let placeHolder = listHeader.getElementsByClassName(placeHolderClass)[0];
-
-    if (placeHolder === undefined){
-      placeHolder = document.createElement('div')
-      placeHolder.className = placeHolderClass;
-      placeHolder.innerText = placeHolderText
-      placeHolder.style.display = 'none';
-      placeHolder.addEventListener('click', () => {
-        listCards.style.display = '';
-         placeHolder.style.display = 'none'
-      })
-      listHeader.append(placeHolder)
-    }
-
     const listCards = list.getElementsByClassName(toHideClass)[0]
 
-    console.log(placeHolder)
-    if ( listCards.style.display === 'none' ){
-      listCards.style.display = '';
-      placeHolder.style.display = 'none'
-    } else {
-      listCards.style.display = 'none';
-      placeHolder.style.display = '';
-    }
+    toggleHidden(listCards, placeHolder);
   }
 }
 
